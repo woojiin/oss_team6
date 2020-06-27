eb80a204c994d35b498f8d3380beeefebe9edb9b
TashaKim-MJ <mijookim.tasha@gmail.com>
2020-06-27 22:34:34 +0900

    modify app.py for elasticsearch

--- app.py
+++ app.py
@@ -18,20 +18,29 @@ def insert_data():
     error = None
     if request.method == 'POST':
         data_list = {}
+        count = 0
+
+        # url 받아오기 
         url = request.form['url']
+
+        # 크롤링
         cr.main(url)
-        # data = {
-        #     "url" = url,
-        #     "time" = 0,
-        #     "count" = len(cr.result1),
-        #     "words" = cr.result1
-        # }
+
+        # 엘라스틱 서치에 데이터 넣기
         data = {}
         data["url"] = url
         data["time"] = 0
         data["count"] = len(cr.result1)
         data["words"] = cr.result1
+        data["flag"] = 1
         res = es.index(index='word', doc_type='url_Data', body=data)
+
+        # 엘라스틱 서치에서 모든 URL을 읽어와서 data_list에 담기
+
+        temp = {}
+
+        data_list[count] = temp
+        count = count + 1
 
         return render_template('index.html', data_list=data_list)
 
--- static/js/table.js
+++ static/js/table.js
@@ -1,61 +1,32 @@
-function parsingdata() {
-    var start = 1946;
-    var end = 2019;
-    var condition1 = $("select[name=grad]").val();
-    var condition2 = $("select[name=gender]").val();
-    console.log(condition1, condition2);
-    var data = [];
-    var depth = 4;
-    parsing1(condition1, condition2, data, depth, start, end);
-  }
-
-   function parsing1(_condition1, _condition2, _data, _depth, _start, _end) {
-    var filename = "area_chart_data_v2.json";
-    var temp = {};
-    $.getJSON("../../json/" + filename, jsonData => {
-      for (var i = 0; i < jsonData.length; i++) 
-      {
-        temp["year"] = jsonData[i]["year"];
-        if (_condition2 == all){
-            temp["college"] = jsonData[i][_condition1 + "_college"];
-            temp["master"] = jsonData[i][_condition1 + "_master"];
-            temp["doctor"] = jsonData[i][_condition1 + "_doctor"];
-        }
-        else{
-            temp["college"] = jsonData[i][_condition1 + "_" + _condition2 + "_college"];
-            temp["master"] = jsonData[i][_condition1 + "_" + _condition2 + "_master"];
-            temp["doctor"] = jsonData[i][_condition1 + "_" + _condition2 + "_doctor"];
-        }
-        _data.push(temp);
-      }
-        drawTable(_data, _depth, _start, _end);
-    });
-  }
-
-   function drawTable(_data, cols, startY, endY) {
-    var array = 
-
-    console.log(_data);
-    console.log(cols);
-    var rows = endY - startY + 1;
-    var temp;
+function drawTable(_data) {
     var HTML = "<table border=1 width=1000>";
     HTML += "<tr>";
     HTML += "<td>구분</td>";
-    HTML += "<td>학사</td>";
-    HTML += "<td>석사</td>";
-    HTML += "<td>박사</td>";
+    HTML += "<td>전체 단어 수</td>";
+    HTML += "<td>처리시간</td>";
+    HTML += "<td>단어 분석</td>";
+    HTML += "<td>유사도 분석</td>";
     HTML += "</tr>";
 
-     for(i=0; i<rows; i++)   { 
-        HTML += "<tr>";
-        HTML += "<td>" + _data[i]["year"] + "</td>";
-        HTML += "<td>" + _data[i]["college"] + "</td>";
-        HTML += "<td>" + _data[i]["master"] + "</td>";
-        HTML += "<td>" + _data[i]["doctor"] + "</td>";
-        HTML += "</tr>" ;
-    }
-
+    //  for(i=0; i<rows; i++)   { 
+    //     HTML += "<tr>";
+    //     HTML += "<td>" + i+1 + "</td>";
+    //     HTML += "<td>" + _data[i]["url"] + "</td>";
+    //     HTML += "<td>" + _data[i]["time"] + "</td>";
+    //     HTML += "<td>" + "<form method="POST" action="/words_func"><input type=submit value="단어 분석"></form>" + "</td>";
+    //     HTML += "<td>" + "<form method="POST" action="/cosine_func"><input type=submit value="유사도 분석"></form>" + "</td>";
+    //     HTML += "</tr>" ;
+    // }
+    
+    for(var temp in _data)   { 
+      HTML += "<tr>";
+      HTML += "<td>" + i+1 + "</td>";
+      HTML += "<td>" + temp[i]["url"] + "</td>";
+      HTML += "<td>" + temp[i]["time"] + "</td>";
+      HTML += "<td>" + "<form method="POST" action="/words_func"><input type=submit value="단어 분석"></form>" + "</td>";
+      HTML += "<td>" + "<form method="POST" action="/cosine_func"><input type=submit value="유사도 분석"></form>" + "</td>";
+      HTML += "</tr>" ;
+  }
      HTML +=("</table>");
     document.getElementById("tablediv").innerHTML = HTML;
 
